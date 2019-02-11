9e0fa126d67232fa0d3f18042a8feff738fb2aa8
Risto Kekovic <risto.kekovic@logeecom.com>
2019-02-11 15:37:58 +0100

    Set order draft source from integration name in configuration
    ISSUE: PLPS-76

--- src/BusinessLogic/Order/OrderService.php
+++ src/BusinessLogic/Order/OrderService.php
@@ -93,7 +93,7 @@ class OrderService extends BaseService
         $draft->contentValueCurrency = $order->getCurrency();
         $draft->contentValue = $order->getTotalPrice();
         $draft->priority = $order->isHighPriority();
-        $draft->source = 'source_inbound';
+        $draft->source = 'module_' . strtolower($this->configuration->getIntegrationName());
 
         $shipping = $order->getShipping();
         if ($shipping !== null) {
