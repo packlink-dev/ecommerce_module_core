0e95f421bc4d09cb028e37227cfd10891025c29d
igor.pesic <igor.pesic@logeecom.com>
2020-05-22 09:29:02 +0200

    Add controllers, services and repositories
    Issue: PLCR1801-6

---
+++ src/DemoUI/src/Controllers/OrderStateMappingController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Packlink\DemoUI\Controllers;
+
+use Packlink\DemoUI\Controllers\Models\Request;
+
+/**
+ * Class OrderStateMappingController
+ * @package Packlink\DemoUI\Controllers
+ */
+class OrderStateMappingController
+{
+    /**
+     * @param \Packlink\DemoUI\Controllers\Models\Request $request
+     */
+    public function getMappings(Request $request)
+    {
+
+    }
+
+    /**
+     * @param \Packlink\DemoUI\Controllers\Models\Request $request
+     */
+    public function setMappings(Request $request)
+    {
+
+    }
+
+    /**
+     * @param \Packlink\DemoUI\Controllers\Models\Request $request
+     */
+    public function getSystemOrderStatuses(Request $request)
+    {
+
+    }
+}
