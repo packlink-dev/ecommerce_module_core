dad9b0e40fad8bc3d110e876336d2c9fb34e73e1
igor.pesic <igor.pesic@logeecom.com>
2020-06-25 11:21:48 +0200

    Add onboarding overview page
    Issue: PLCR1801-28

---
+++ src/BusinessLogic/Resources/js/OnboardingOverviewController.js
@@ -0,0 +1,81 @@
+var Packlink = window.Packlink || {};
+
+(function () {
+    function OnboardingOverviewController(configuration) {
+
+        const templateService = Packlink.templateService,
+            ajaxService = Packlink.ajaxService,
+            templateId = 'pl-onboarding-overview-page';
+
+        let defaultParcel,
+            defaultWarehouse;
+
+        /**
+         * Displays page content.
+         */
+        this.display = function () {
+            ajaxService.get(configuration.defaultParcelGet, fetchDefaultWarehouse);
+        };
+
+        function fetchDefaultWarehouse(response) {
+            defaultParcel = response;
+            ajaxService.get(configuration.defaultWarehouseGet, initializePage);
+        }
+
+        function initializePage(response) {
+            defaultWarehouse = response;
+            templateService.setCurrentTemplate(templateId);
+            let submitBtn = templateService.getComponent('pl-onboarding-overview-button');
+
+            if (defaultParcel && defaultParcel.hasOwnProperty('weight') && defaultParcel.weight !== null) {
+                setRequiredInfoPopulatedState('pl-parcel-details', 'pl-parcel-wrapper');
+
+                let weightPlaceholder = templateService.getComponent('pl-parcel-weight'),
+                    heightPlaceholder = templateService.getComponent('pl-parcel-height'),
+                    widthPlaceholder = templateService.getComponent('pl-parcel-width'),
+                    lengthPlaceholder = templateService.getComponent('pl-parcel-length');
+
+                weightPlaceholder.innerHTML = defaultParcel.weight;
+                heightPlaceholder.innerHTML = defaultParcel.height;
+                widthPlaceholder.innerHTML = defaultParcel.width;
+                lengthPlaceholder.innerHTML = defaultParcel.length;
+            } else {
+                setMissingInfoState('pl-parcel-wrapper');
+            }
+
+            if (defaultWarehouse && defaultWarehouse.hasOwnProperty('name') && defaultWarehouse.name !== null) {
+                setRequiredInfoPopulatedState('pl-wh-details', 'pl-wh-wrapper');
+
+                let aliasPlaceholder = templateService.getComponent('pl-wh-alias'),
+                    userPlaceholder = templateService.getComponent('pl-wh-user'),
+                    companyPlaceholder = templateService.getComponent('pl-wh-company');
+
+                aliasPlaceholder.innerHTML = defaultWarehouse.alias;
+                userPlaceholder.innerHTML = defaultWarehouse.name + ' ' + defaultWarehouse.surname;
+                companyPlaceholder.innerHTML = defaultWarehouse.company;
+            } else {
+                setMissingInfoState('pl-wh-wrapper');
+            }
+
+            function setMissingInfoState(parentClass) {
+                let missingInfo = document.querySelector('.' + parentClass + ' .pl-onboarding-missing-info');
+                missingInfo.classList.remove('pl-display-none');
+
+                let indicator = document.querySelector('.' + parentClass + ' .pl-info-icon-x');
+                indicator.classList.remove('pl-display-none');
+
+                submitBtn.disabled = true;
+            }
+        }
+
+        function setRequiredInfoPopulatedState(detailsWrapperId, indicatorWrapperClass) {
+            let parcelInfo = templateService.getComponent(detailsWrapperId);
+            parcelInfo.classList.remove('pl-display-none');
+
+            let indicator = document.querySelector('.' + indicatorWrapperClass + ' .pl-info-icon-ok');
+            indicator.classList.remove('pl-display-none');
+        }
+    }
+
+    Packlink.OnboardingOverviewController = OnboardingOverviewController;
+})();
--- src/DemoUI/src/Controllers/DefaultParcelController.php
+++ src/DemoUI/src/Controllers/DefaultParcelController.php
@@ -2,20 +2,31 @@
 
 namespace Packlink\DemoUI\Controllers;
 
+use Logeecom\Infrastructure\Configuration\Configuration;
+use Logeecom\Infrastructure\ServiceRegister;
 use Packlink\DemoUI\Controllers\Models\Request;
+use Packlink\DemoUI\Services\BusinessLogic\ConfigurationService;
 
 /**
  * Class DefaultParcelController
+ *
  * @package Packlink\DemoUI\Controllers
  */
 class DefaultParcelController
 {
     /**
+     * @var ConfigurationService
+     */
+    private $configService;
+
+    /**
      * @param \Packlink\DemoUI\Controllers\Models\Request $request
      */
-    public function getDefaultParcel(Request $request)
+    public function getDefaultParcel()
     {
+        $parcel = $this->getConfigService()->getDefaultParcel();
 
+        echo json_encode($parcel ? $parcel->toArray() : array());
     }
 
     /**
@@ -24,5 +35,19 @@ class DefaultParcelController
     public function setDefaultParcel(Request $request)
     {
 
+    }
+
+    /**
+     * Returns an instance of configuration service.
+     *
+     * @return ConfigurationService
+     */
+    protected function getConfigService()
+    {
+        if ($this->configService === null) {
+            $this->configService = ServiceRegister::getService(Configuration::CLASS_NAME);
+        }
+
+        return $this->configService;
     }
 }
