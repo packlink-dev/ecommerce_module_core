895d29879e7ed2e9eb8b0fe1648aa11a94c91653
Risto <risto.kekovic@logeecom.com>
2021-06-24 10:49:45 +0200

    Simplify unit test for grouping of the services based on the currency
    ISSUE: PLCS26-9

--- tests/BusinessLogic/Tasks/UpdateShippingServicesTaskTest.php
+++ tests/BusinessLogic/Tasks/UpdateShippingServicesTaskTest.php
@@ -219,41 +219,24 @@ class UpdateShippingServicesTaskTest extends BaseSyncTest
      * @throws \Logeecom\Infrastructure\ORM\Exceptions\RepositoryNotRegisteredException
      * @throws \Packlink\BusinessLogic\DTO\Exceptions\FrontDtoValidationException
      */
-    public function testServiceDoesNotBelongToMethodByCurrency()
+    public function testShippingServiceGroupingByCurrency()
     {
         $this->prepareAndExecuteValidTask();
 
-        $methodId = 4;
-        // update locally first
-        $this->shippingMethodService->activate($methodId);
-        $method = $this->shippingMethodService->getShippingMethod($methodId);
-        $services = $method->getShippingServices();
-        // previous price 5.98
-        $services[0]->totalPrice = 14.27;
-        $method->setShippingServices($services);
-        // add new service
-        $method->addShippingService(new ShippingService(
-            12345,
-            'new service',
-            'IT',
-            'IT',
-            12.43,
-            10.40,
-            2.03
-        ));
-        $method->setCurrency('GBP');
+        $methods = $this->shippingMethodService->getAllMethods();
         $repo = RepositoryRegistry::getRepository(ShippingMethod::CLASS_NAME);
-        $repo->update($method);
+        foreach ($methods as $method) {
+            $method->setCurrency('GBP');
+            $repo->update($method);
+        }
 
-        $method = $this->shippingMethodService->getShippingMethod($methodId);
-        self::assertCount(2, $method->getShippingServices());
+        $this->prepareAndExecuteValidTask();
+        $methods = $this->shippingMethodService->getAllMethods();
 
-        // execute task once more. All services should be updated and invalid services should be deleted.
-        $this->httpClient->setMockResponses($this->getValidMockResponses());
-        $this->syncTask->execute();
-
-        self::assertCount(18, $this->shippingMethodService->getAllMethods());
-        self::assertEmpty($this->shippingMethodService->getActiveMethods());
+        self::assertCount(18, $methods);
+        foreach ($methods as $method) {
+            self::assertEquals('EUR', $method->getCurrency());
+        }
     }
 
     /**
