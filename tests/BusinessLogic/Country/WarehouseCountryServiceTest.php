d016cd48e3c5b4488ccfd2701eaef9ab1a9b80e6
Sofija Dokmanovic <ivan.bojovic@sendcloud.com>
2024-12-06 16:28:19 +0100

    Add unsupported country Japan
    ISSUE: CS-6346

--- tests/BusinessLogic/Country/WarehouseCountryServiceTest.php
+++ tests/BusinessLogic/Country/WarehouseCountryServiceTest.php
@@ -23,7 +23,7 @@ class WarehouseCountryServiceTest extends BaseTestWithServices
         $countries = $service->getSupportedCountries();
 
         $this->assertNotEmpty($countries);
-        $this->assertCount(42, $countries);
+        $this->assertCount(43, $countries);
         $this->assertArrayHasKey('MX', $countries);
         $this->assertEquals('Mexico', $countries['MX']->name);
         $this->assertEquals('MX', $countries['MX']->code);
